5a0adc0f7efeaac1f1b2772d4246e219a42f2bfb
Manuelic <manuel-ic@outlook.com>
2019-06-08 17:12:09 +0100

    Clase 21

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Empleado} from './empleado';
+import { from } from 'rxjs';
 
 @Component({
     selector: 'empleado',
@@ -6,6 +8,21 @@ import {Component} from '@angular/core';
 })
 
 export class EmpleadoComponent{
-    public nombre_componente = 'Componente de empleado';
+    public nombre_componente = 'Componente de empleados: ';
     public listado_empleados = 'Pepe, Andrés, Carolina y Paula';
+    public empleado:Empleado;
+    public trabajadores:Array<Empleado>;
+
+    constructor(){
+        this.empleado = new Empleado('Pepe López', 46, 'técnico', true);
+        this.trabajadores = [
+            new Empleado('Pepe López', 46, 'técnico', true),
+            new Empleado('Sara Martín', 27, 'recepcionista', true),
+            new Empleado('José Perez', 67, 'mantenimiento', false)
+        ];
+    }
+
+    ngOnInit(){
+        
+    }
 }
