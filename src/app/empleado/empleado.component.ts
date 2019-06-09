6dd179279fda7557ab4ac3684dd3958874c9c08d
Manuelic <manuel-ic@outlook.com>
2019-06-09 12:54:59 +0100

    Clases 24, 25, 26 y 27

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -12,17 +12,27 @@ export class EmpleadoComponent{
     public listado_empleados = 'Pepe, Andrés, Carolina y Paula';
     public empleado:Empleado;
     public trabajadores:Array<Empleado>;
+    public trabajador_externo:boolean;
+    public color:string;
+    public color_seleccionado:string;
 
     constructor(){
-        this.empleado = new Empleado('Pepe López', 46, 'técnico', true);
+        this.empleado = new Empleado('Raul del Pozo', 34, 'profesor', false);
         this.trabajadores = [
             new Empleado('Pepe López', 46, 'técnico', true),
             new Empleado('Sara Martín', 27, 'recepcionista', true),
             new Empleado('José Perez', 67, 'mantenimiento', false)
         ];
+        this.trabajador_externo = true;
+        this.color = 'blue';
+        this.color_seleccionado = 'yellow';
     }
 
     ngOnInit(){
         
+    }
+
+    cambiarExterno(valor){
+        this.trabajador_externo = valor;
     }
 }
