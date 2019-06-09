9522f6bf3e825aee9c340973a97f2bcd5fe7667a
Manuelic <manuel-ic@outlook.com>
2019-06-09 17:37:09 +0100

    Clases 31 y 32

---
+++ src/app/contacto/contacto.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'contacto',
+    templateUrl: './contacto.component.html'
+})
+
+export class ContactoComponent{
+    public titulo = 'Página De Contacto de la Web';
+
+    constructor(){
+
+    }
+
+    ngOnInit(){
+
+    }
+}
